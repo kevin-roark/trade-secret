89f7a823cf04ff21399f02450f718c3b769b050b
Kevin Roark <ker2143@columbia.edu>
2014-03-27 18:26:52 -0400

    holy cow' '

--- js/main.js
+++ js/main.js
@@ -22,6 +22,23 @@ var phrases = [
   "Need To Make Small Talk In a Business Setting? Learn The Tricks of the Trade for Seeming Like A Well-Rounded Person"
 ];
 
+var images = [
+  'images/networking/1.jpg',
+  'images/networking/2.jpg',
+  'images/networking/3.jpg',
+  'images/networking/4.jpg',
+  'images/networking/5.jpg',
+  'images/networking/6.jpg',
+  'images/networking/7.png',
+  'images/networking/8.jpg',
+  'images/networking/9.jpg',
+  'images/networking/10.jpg',
+  'images/networking/11.jpg',
+  'images/networking/12.jpg',
+  'images/networking/13.jpg',
+  'images/networking/14.jpg'
+];
+
 var attemptedClicks = 0;
 var numLinks = 1;
 
@@ -32,17 +49,24 @@ $('.initial-link').mouseover(function() {
   linkMove(self);
   flashUnderline(self, 300);
   self.unbind('mouseover');
+  getKevinsGoing();
 });
 
 $(document).on('mouseover', '.network-link', linkHovered);
 $(document).on('click', '.network-link', linkHovered);
+$(document).on('mouseover', '.network-image', imageHovered);
+$(document).on('click', '.network-image', imageHovered);
 
 function linkHovered() {
   linkMove($(this));
 }
 
+function imageHovered() {
+  imageMove($(this));
+}
+
 function linkMove(el) {
-  var left = Math.floor(Math.random() * $(window).width() * 0.8);
+  var left = Math.floor(Math.random() * $(window).width() * 0.6);
   var top = Math.floor(Math.random() * ($(window).height() - 200)) + 200;
   var content = kutility.choice(phrases);
   attemptedClicks++;
@@ -56,15 +80,37 @@ function linkMove(el) {
   increaseFontsize(el);
   kutility.addShadow(el, 10);
 
-  if (attemptedClicks % 10 == 0 && numLinks < 10) {
-    createLink();
+  addSomething();
+}
+
+function imageMove(img) {
+  var left = Math.floor(Math.random() * $(window).width() * 0.8);
+  var top = Math.floor(Math.random() * ($(window).height() - 200)) + 200;
+  attemptedClicks++;
+
+  img.css('left', left);
+  img.css('top', top);
+  img.css('z-index', attemptedClicks);
+
+  kutility.addShadow(img, 10);
+
+  addSomething();
+}
+
+function addSomething() {
+  if (attemptedClicks % 10 == 0 && numLinks < 20) {
+    var p = Math.random();
+    if (p < 0.69)
+      createLink();
+    else
+      createImage();
     numLinks++;
   }
 }
 
 function increaseFontsize(el) {
   var size = el.css('font-size');
-  size.replace('px', '');
+  size = size.replace('px', '');
   var num = parseInt(size);
   num = parseInt(num *= 1.04);
   el.css('font-size', num + 'px');
@@ -97,4 +143,72 @@ function createLink() {
   linkMove(link);
 
   flashUnderline(link, 300);
+}
+
+function createImage() {
+  var image = $('<div class="network-image">');
+  var img = $('<img src="' + kutility.choice(images) + '">');
+  image.append(img);
+
+  var bg = kutility.randColor();
+  bg = kutility.alphize(bg, 0.95);
+  image.css('background-color', bg);
+
+  $('body').append(image);
+  imageMove(image);
+
+  warpLater(image);
+}
+
+function warpLater(el) {
+  setTimeout(function() {
+    kutility.warp(el);
+  }, 1000);
+}
+
+function getKevinsGoing() {
+  setTimeout(function() {
+    var k1 = $('.kevin-image-1');
+    k1.fadeIn();
+
+    var sat = 100;
+    var satTimer = setInterval(function() {
+      kutility.saturate(k1, sat++);
+      kutility.contrast(k1, sat);
+      if (sat > 450) {
+        clearInterval(satTimer);
+        doHue();
+      }
+    }, 60);
+
+    function doHue() {
+      var hue = 0;
+      setInterval(function() {
+        kutility.hutate(k1, hue++);
+      }, 30);
+    }
+
+  }, 10000);
+
+  setTimeout(function() {
+    var k2 = $('.kevin-image-2');
+    k2.fadeIn();
+
+    var inv = 0;
+    var invTimer = setInterval(function() {
+      kutility.invert(k2, inv++);
+      if (inv > 100)
+        clearInterval(invTimer);
+    }, 150);
+
+    setTimeout(function() {
+      var b = 0;
+      var bTimer = setInterval(function() {
+        kutility.brightness(k2, b++);
+        if (b > 350)
+          clearInterval(bTimer);
+      }, 35);
+    }, 9000);
+
+  }, 25000);
 }
--- js/main_build.js
+++ js/main_build.js
@@ -93,6 +93,307 @@ Kutility.prototype.addShadow = function(el, size) {
   el.css('box-shadow', shadow);
 }
 
+/**
+ * Add transform to element with all the lame browser prefixes.
+ *
+ * @api public
+ */
+Kutility.prototype.addTransform = function(el, transform) {
+  var curTransform = this.getTransform(el);
+  curTransform = curTransform.replace('none', '');
+  var newTransform = curTransform + transform;
+  this.setTransform(el, newTransform);
+}
+
+/**
+ * Set transform of element with all the lame browser prefixes.
+ *
+ * @api public
+ */
+Kutility.prototype.setTransform = function(el, transform) {
+  el.css('-webkit-transform', transform);
+  el.css('-moz-transform', transform);
+  el.css('-ms-transform', transform);
+  el.css('-o-transform', transform);
+  el.css('transform', transform);
+}
+
+/**
+ * Check an elements tansform.
+ *
+ * @api public
+ */
+Kutility.prototype.getTransform = function(el) {
+  var possible = ['transform', '-webkit-transform', '-moz-transform', '-ms-transform', '-o-transform'];
+
+  for (var i = 0; i < possible.length; i++) {
+    var f = el.css(possible[i]);
+    if (f == 'none' && i + 1 < possible.length) {
+      var pf = el.css(possible[i + 1]);
+      if (pf)
+        continue;
+    }
+    return f;
+  }
+}
+
+/**
+ * Remove all transforms from element.
+ *
+ * @api public
+ */
+Kutility.prototype.clearTransforms = function(el) {
+  el.css('-webkit-transform', '');
+  el.css('-moz-transform', '');
+  el.css('-ms-transform', '');
+  el.css('-o-transform', '');
+  el.css('transform', '');
+}
+
+/**
+ * Rotate an element by x degrees.
+ *
+ * @api public
+ */
+Kutility.prototype.rotate = function(el, x) {
+  var ct = this.getTransform(el);
+  ct = ct.replace(/matrix\(.*\)/, '').replace('none', '');
+
+  var t = ' rotate(' + x + 'deg)';
+  this.setTransform(el, ct  + t);
+}
+
+/**
+ * Scale an element by x (no units);
+ *
+ * @api public
+ */
+Kutility.prototype.scale = function(el, x) {
+  var ct = this.getTransform(el);
+  ct = ct.replace(/scale\(.*\)/, '').replace('none', '');
+
+  var t = ' scale(' + x + ')';
+  this.setTransform(el, ct + t);
+}
+
+/**
+ * Translate an element by x, y (include your own units);
+ *
+ * @api public
+ */
+Kutility.prototype.translate = function(el, x, y) {
+  var ct = this.getTransform(el);
+  ct = ct.replace(/translate\(.*\)/, '').replace('none', '');
+
+  var t = ' translate(' + x + ', '  + y + ')';
+  this.setTransform(el, ct + t);
+}
+
+/**
+ * Skew an element by x, y degrees;
+ *
+ * @api public
+ */
+Kutility.prototype.skew = function(el, x, y) {
+  var ct = this.getTransform(el);
+  ct = ct.replace(/skew\(.*\)/, '').replace('none', '');
+
+  var xd = x + 'deg';
+  var yd = y + 'deg';
+  var t = ' skew(' + xd + ', ' + yd + ')';
+  this.setTransform(el, ct + t);
+}
+
+/**
+ * Warp an element by a random amount by rotating and skewing it.
+ *
+ * @api public
+ */
+Kutility.prototype.warp = function(el) {
+  var r = Math.floor(Math.random() * 360);
+  var x = Math.floor(Math.random() * 360);
+  var y = Math.floor(Math.random() * 360);
+
+  this.rotate(el, r);
+  this.skew(el, x, y);
+}
+
+/**
+ * Add filter to element with all the lame browser prefixes.
+ *
+ * @api public
+ */
+Kutility.prototype.addFilter = function(el, filter) {
+  var curFilter = this.getFilter(el);
+  curFilter = curFilter.replace('none', '');
+  var newFilter = curFilter + ' ' + filter;
+  this.setFilter(el, newFilter);
+}
+
+/**
+ * Set filter to element with all lame prefixes.
+ *
+ * @api public
+ */
+Kutility.prototype.setFilter = function(el, filter) {
+  el.css('-webkit-filter', filter);
+  el.css('-moz-filter', filter);
+  el.css('-ms-filter', filter);
+  el.css('-o-filter', filter);
+  el.css('filter', filter);
+}
+
+/**
+ * Check an elements filter.
+ *
+ * @api public
+ */
+Kutility.prototype.getFilter = function(el) {
+  var possible = ['filter', '-webkit-filter', '-moz-filter', '-ms-filter', '-o-filter'];
+
+  for (var i = 0; i < possible.length; i++) {
+    var f = el.css(possible[i]);
+    if (f == 'none' && i + 1 < possible.length) {
+      var pf = el.css(possible[i + 1]);
+      if (pf)
+        continue;
+    }
+    return f;
+  }
+}
+
+/**
+ * Remove all filters from element.
+ *
+ * @api public
+ */
+Kutility.prototype.clearFilters = function(el) {
+  el.css('-webkit-filter', '');
+  el.css('-moz-filter', '');
+  el.css('-ms-filter', '');
+  el.css('-o-filter', '');
+  el.css('filter', '');
+}
+
+/**
+
+/**
+ * Grayscale an element by x percent.
+ *
+ * @api public
+ */
+Kutility.prototype.grayscale = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/grayscale\(.*\)/, '').replace('none', '');
+
+  var f = ' grayscale(' + x + '%)';
+  this.setFilter(el, cf  + f);
+}
+
+/**
+ * Sepia an element by x percent.
+ *
+ * @api public
+ */
+Kutility.prototype.sepia = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/sepia\(.*\)/, '').replace('none', '');
+
+  var f = ' sepia(' + x + '%)';
+  this.setFilter(el, cf + f);
+}
+
+/**
+ * Saturate an element by x percent.
+ *
+ * @api public
+ */
+Kutility.prototype.saturate = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/saturate\(.*\)/, '').replace('none', '');
+
+  var f = ' saturate(' + x + '%)';
+  this.setFilter(el, cf + f);
+}
+
+/**
+ * Invert an element by x percent.
+ *
+ * @api public
+ */
+Kutility.prototype.invert = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/invert\(.*\)/, '').replace('none', '');
+
+  var f = ' invert(' + x + '%)';
+  this.setFilter(el, cf + f);
+}
+
+/**
+ * Hue-rotate an element by x degrees.
+ *
+ * @api public
+ */
+Kutility.prototype.hutate = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/hue-rotate\(.*\)/, '').replace('none', '');
+
+  var f = ' hue-rotate(' + x + 'deg)';
+  this.setFilter(el, cf + f);
+}
+
+/**
+ * Set opacity of an element to x percent.
+ *
+ * @api public
+ */
+Kutility.prototype.opace = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/opacity\(.*\)/, '').replace('none', '');
+
+  var f = ' opacity(' + x + '%)';
+  this.setFilter(el, cf + f);
+}
+
+/**
+ * Set brightness of an element to x percent.
+ *
+ * @api public
+ */
+Kutility.prototype.brightness = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/brightness\(.*\)/, '').replace('none', '');
+
+  var f = ' brightness(' + x + '%)';
+  this.setFilter(el, cf + f);
+}
+
+/**
+ * Set contrast of an element to x percent.
+ *
+ * @api public
+ */
+Kutility.prototype.contrast = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/contrast\(.*\)/, '').replace('none', '');
+
+  var f = ' contrast(' + x + '%)';
+  this.setFilter(el, cf + f);
+}
+
+/**
+ * Blur an element by x pixels.
+ *
+ * @api public
+ */
+Kutility.prototype.blur = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/blur\(.*\)/, '').replace('none', '');
+
+  var f = ' blur(' + x + 'px)';
+  this.setFilter(el, cf + f);
+}
+
 },{}],2:[function(require,module,exports){
 
 var kutility = require('./lib/kutility'); /* you can remove this if you don't want it */
@@ -118,6 +419,23 @@ var phrases = [
   "Need To Make Small Talk In a Business Setting? Learn The Tricks of the Trade for Seeming Like A Well-Rounded Person"
 ];
 
+var images = [
+  'images/networking/1.jpg',
+  'images/networking/2.jpg',
+  'images/networking/3.jpg',
+  'images/networking/4.jpg',
+  'images/networking/5.jpg',
+  'images/networking/6.jpg',
+  'images/networking/7.png',
+  'images/networking/8.jpg',
+  'images/networking/9.jpg',
+  'images/networking/10.jpg',
+  'images/networking/11.jpg',
+  'images/networking/12.jpg',
+  'images/networking/13.jpg',
+  'images/networking/14.jpg'
+];
+
 var attemptedClicks = 0;
 var numLinks = 1;
 
@@ -128,17 +446,24 @@ $('.initial-link').mouseover(function() {
   linkMove(self);
   flashUnderline(self, 300);
   self.unbind('mouseover');
+  getKevinsGoing();
 });
 
 $(document).on('mouseover', '.network-link', linkHovered);
 $(document).on('click', '.network-link', linkHovered);
+$(document).on('mouseover', '.network-image', imageHovered);
+$(document).on('click', '.network-image', imageHovered);
 
 function linkHovered() {
   linkMove($(this));
 }
 
+function imageHovered() {
+  imageMove($(this));
+}
+
 function linkMove(el) {
-  var left = Math.floor(Math.random() * $(window).width() * 0.8);
+  var left = Math.floor(Math.random() * $(window).width() * 0.6);
   var top = Math.floor(Math.random() * ($(window).height() - 200)) + 200;
   var content = kutility.choice(phrases);
   attemptedClicks++;
@@ -152,15 +477,37 @@ function linkMove(el) {
   increaseFontsize(el);
   kutility.addShadow(el, 10);
 
-  if (attemptedClicks % 10 == 0 && numLinks < 10) {
-    createLink();
+  addSomething();
+}
+
+function imageMove(img) {
+  var left = Math.floor(Math.random() * $(window).width() * 0.8);
+  var top = Math.floor(Math.random() * ($(window).height() - 200)) + 200;
+  attemptedClicks++;
+
+  img.css('left', left);
+  img.css('top', top);
+  img.css('z-index', attemptedClicks);
+
+  kutility.addShadow(img, 10);
+
+  addSomething();
+}
+
+function addSomething() {
+  if (attemptedClicks % 10 == 0 && numLinks < 20) {
+    var p = Math.random();
+    if (p < 0.69)
+      createLink();
+    else
+      createImage();
     numLinks++;
   }
 }
 
 function increaseFontsize(el) {
   var size = el.css('font-size');
-  size.replace('px', '');
+  size = size.replace('px', '');
   var num = parseInt(size);
   num = parseInt(num *= 1.04);
   el.css('font-size', num + 'px');
@@ -193,6 +540,74 @@ function createLink() {
   linkMove(link);
 
   flashUnderline(link, 300);
+}
+
+function createImage() {
+  var image = $('<div class="network-image">');
+  var img = $('<img src="' + kutility.choice(images) + '">');
+  image.append(img);
+
+  var bg = kutility.randColor();
+  bg = kutility.alphize(bg, 0.95);
+  image.css('background-color', bg);
+
+  $('body').append(image);
+  imageMove(image);
+
+  warpLater(image);
+}
+
+function warpLater(el) {
+  setTimeout(function() {
+    kutility.warp(el);
+  }, 1000);
+}
+
+function getKevinsGoing() {
+  setTimeout(function() {
+    var k1 = $('.kevin-image-1');
+    k1.fadeIn();
+
+    var sat = 100;
+    var satTimer = setInterval(function() {
+      kutility.saturate(k1, sat++);
+      kutility.contrast(k1, sat);
+      if (sat > 450) {
+        clearInterval(satTimer);
+        doHue();
+      }
+    }, 60);
+
+    function doHue() {
+      var hue = 0;
+      setInterval(function() {
+        kutility.hutate(k1, hue++);
+      }, 30);
+    }
+
+  }, 10000);
+
+  setTimeout(function() {
+    var k2 = $('.kevin-image-2');
+    k2.fadeIn();
+
+    var inv = 0;
+    var invTimer = setInterval(function() {
+      kutility.invert(k2, inv++);
+      if (inv > 100)
+        clearInterval(invTimer);
+    }, 150);
+
+    setTimeout(function() {
+      var b = 0;
+      var bTimer = setInterval(function() {
+        kutility.brightness(k2, b++);
+        if (b > 350)
+          clearInterval(bTimer);
+      }, 35);
+    }, 9000);
+
+  }, 25000);
 }
 
 },{"./lib/kutility":1}]},{},[2])
